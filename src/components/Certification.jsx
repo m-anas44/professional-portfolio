6d3139c97e86340af15908df98cb22e5ec92dc3d
Muhammad Anas <manasansari1438@gmail.com>
2024-11-19 18:26:12 -0800

    grid edited

--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -7,12 +7,12 @@ const Certification = () => {
     <section className="section" id="certificates">
       <div className="container">
         <h2 className="headline-2 mb-8 reveal-up">Certifications</h2>
-        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(350px,_1fr))]">
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
           <div
             className="relative rounded-2xl p-4 hover:bg-zinc-700/50 active:bg-zinc-700/60 ring-1 
         ring-inset ring-zinc-50/5 transition-colors reveal-up"
           >
-            <figure className="img-box aspect-square rounded-lg mb-4">
+            <figure className="rounded-lg mb-4">
               <img
                 src={certificate1}
                 alt="certificate 1"
@@ -28,7 +28,7 @@ const Certification = () => {
             className="relative rounded-2xl p-4 hover:bg-zinc-700/50 active:bg-zinc-700/60 ring-1 
         ring-inset ring-zinc-50/5 transition-colors reveal-up"
           >
-            <figure className="img-box aspect-square rounded-lg mb-4">
+            <figure className="rounded-lg mb-4">
               <img
                 src={certificate2}
                 alt="certificate 2"
